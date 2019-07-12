4135021ee4b32f055e5b5ccc3435a8e3c63ec808
David Millman <dave@cs.unc.edu>
2019-07-11 18:41:52 -0600

    Clean up config managment

--- main.go
+++ main.go
@@ -10,24 +10,29 @@ const CONFIG_LOCAL string = "CONFIG_LOCAL"
 const CONFIG_MAINNET string = "CONFIG_MAINNET"
 const CONFIG string = CONFIG_MAINNET
 
+func url(config string) string {
+	address := "https://mainnet.infura.io/v3/c540c4fe10614f27a05a22bd463f0996"
+	if config == CONFIG_LOCAL {
+		address = "http://localhost:8545"
+	}
+	return address
+}
+
+func accountHex(config string) string {
+	hex := "0x71c7656ec7ab88b098defb751b7401b5f6d8976f"
+	if config == CONFIG_LOCAL {
+		hex = "0xe280029a7867ba5c9154434886c241775ea87e53"
+	}
+	return hex
+}
+
 func main() {
 	log.Println("Running with configuration ", CONFIG)
 
-	url := "https://mainnet.infura.io/v3/c540c4fe10614f27a05a22bd463f0996"
-	if CONFIG == CONFIG_LOCAL {
-		url = "http://localhost:8545"
-	}
-	_ = url
-	client := gethbk.Client(url)
+	client := gethbk.Client(url(CONFIG))
 
-	accountHex := "0x71c7656ec7ab88b098defb751b7401b5f6d8976f"
-	if CONFIG == CONFIG_LOCAL {
-		accountHex = "0xe280029a7867ba5c9154434886c241775ea87e53"
-	}
-	_ = accountHex
-
-	// gethbk.AccountsBalances(client, accountHex)
-	// gethbk.AccountsGeneratingNewWallets()
-	// gethbk.AccountsKeystores()
+	gethbk.AccountsBalances(client, accountHex(CONFIG))
+	gethbk.AccountsGeneratingNewWallets()
+	gethbk.AccountsKeystores()
 	gethbk.AddressCheck(client)
 }
