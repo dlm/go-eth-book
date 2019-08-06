873844f34949052a6feb471a8a829dbde364337e
David Millman <dave@cs.unc.edu>
2019-08-06 14:54:40 -0600

    Add a dial injection for making client

--- gethbk/client.go
+++ gethbk/client.go
@@ -2,13 +2,20 @@ package gethbk
 
 import (
 	"log"
+
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func Client(url string) *ethclient.Client {
+func MakeClient(
+	url string,
+	dial func(string) (*ethclient.Client, error),
+) *ethclient.Client {
 	client, err := ethclient.Dial(url)
 	checkForError(err)
 	log.Println("Connection success")
 	return client
 }
 
+func Client(url string) *ethclient.Client {
+	return MakeClient(url, ethclient.Dial)
+}
